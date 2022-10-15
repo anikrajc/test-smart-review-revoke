754afcc0af09541942433ba46afd65e111947d10
Anik C <anik.raj@swiggy.in>
2022-10-15 21:17:14 +0530

    asd

--- test.sh
+++ test.sh
@@ -4,10 +4,9 @@ str3=""
 # test-srr-start
 if [ ! -z "$str" -a "$str" != " " ]; then
         echo "Str is not null or space"
-fi
 
 if [ ! -z "$str2" -a "$str2" != " " ]; then
-        echo "Str2 is not null or space"
+        echo "Str2 is not null or space"asdasdas
 fi
 # test-srr-end
 if [ ! -z "$str3" -a "$str3" != " " ]; then
