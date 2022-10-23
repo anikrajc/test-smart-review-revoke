2970fc6dd7c503a24b4694524b93e6bd2ebe7fe4
Anik C <anik.raj@swiggy.in>
2022-10-23 15:20:38 +0530

    asd

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 response=$(curl --location --request POST 'https://us-central1-testsrr-495b8.cloudfunctions.net/getApprovedReviews' --header 'Content-Type: application/json' --data-raw '{ "organisation" : "anikrajc", "repository" : "test-smart-review-revoke", "number" : "1" }')
 echo "$response"
+#  test-srr-start
 if [ -n "$response" ]; then
     commitId=$(echo "$response" | sed -n 1p)
     reviewIds=$(echo "$response" | sed -n 2p)
     echo "commit id is $commitId"
     echo "reviewIds id is $reviewIds"
+# test-srr-end
 else
     echo "empty"
 fi
