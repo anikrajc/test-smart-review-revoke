436dcbeb38effcd8ad6c84c3ca3d74b34005019f
Anik C <anik.raj@swiggy.in>
2022-10-30 16:32:30 +0530

    asd

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ if [ -n "$response" ]; then
     commitId=$(echo "$response" | sed -n 1p)
     reviewIds=$(echo "$response" | sed -n 2p)
     echo "commit id is $commitIdasd
-    echo "reviewIds id is $reviewIds"
+    echo "reviewIds id is $reviewIds"as das da s
 # test-srr-end
 else
     echo "empty"
